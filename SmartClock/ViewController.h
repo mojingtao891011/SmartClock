05e8f2f5a77f97e8495e1b9601d25e3e78aa24fe
taomojingato <mojingtao@taomojingatodeMacBook-Pro.local>
2015-04-29 15:30:43 +0800

    Initial Commit

---
+++ SmartClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartClock
+//
+//  Created by taomojingato on 15/4/29.
+//  Copyright (c) 2015年 mojingato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
